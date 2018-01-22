4233164200ce8a22915dc5c073eea2cff7daa87c
Lasarian <anonimduma@gmail.com>
2018-01-22 10:12:53 +0200

    Task_13

--- initializr/js/main.js
+++ initializr/js/main.js
@@ -1,1 +1,52 @@
 
+function test() {
+    return $("#textul").val();
+}
+console.log(test());
+
+console.log('inputup are valoarea', $("#textul").val());
+$("#div1 h1").text();
+
+
+
+
+
+var i = 1;
+window.setInterval(pluteste, 500);
+function pluteste() {
+
+
+        if (i === 1) {
+            $(".middleSection .col:last-child img").attr('src', "https://cdn3.iconfinder.com/data/icons/picons-weather/57/15_heavy_rain-512.png");
+        } else
+            if(i === 20){
+            $(".middleSection .col:last-child img").attr('src', "https://cdn3.iconfinder.com/data/icons/flat-icons-web/40/Sun-512.png");
+        }
+        else
+            if(i === 40) {
+                $(".middleSection .col:last-child img").attr('src', "https://img00.deviantart.net/358c/i/2015/338/b/7/vector__3_rainbow_cloud_cutie_mark_by_greenmachine987-d8e65kf.png");
+            }
+            else
+            if(i === 60) {
+                i = i * -1;
+            }
+
+
+
+
+    i = i + 1;
+
+    $(".middleSection .col:last-child").animate(i > -1 ? {
+        top: "-=20px",
+        left: "-=20px"
+    } : {
+        top: "+=20px",
+        left: "+=20px"
+    }, 300);
+
+    $("#buton1").click(function(){
+        $( this ).slideUp();
+    })
+
+
+}
